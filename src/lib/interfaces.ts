1c671d1c8b1ceb2819170e45227a78d025711447
Swapnil <swapmis1084@gmail.com>
2019-05-30 14:46:56 +0200

    Moved interface to separate file.

---
+++ src/lib/interfaces.ts
@@ -0,0 +1,11 @@
+export interface ConfigReader {
+  setConfigName(fileName: string): void;
+  addConfigPath(path: string): void;
+  readInConfig(): { error?: Error };
+  getString(propertyPath: string): string;
+  getInt(propertyPath: string): number;
+  getBoolean(propertyPath: string): boolean;
+  getDate(propertyPath: string): Date;
+  getStringArray(propertyPath: string): Array<string>;
+  getIntArray(propertyPath: string): Array<number>;
+}
